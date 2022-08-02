e3e945b1ec0c0c8cbbdff995d694381b7bf9030b
patribu7 <patribu7@gmail.com>
2022-08-02 23:44:21 +0200

    Prepare function changeValue()
    Test with buttonPlus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-function makeElement(type, text, className) {
+number = document.getElementById('number')
+
+function makeElement(type, text, id) {
   const element = document.createElement(type);
   element.innerText = text;
-  element.classList.add(className);
+  element.setAttribute('id', id);
   document.getElementById('container').appendChild(element);
+};
+
+function changeValue(operation, value) {
+  switch (operation) {
+    case 'incr':
+      number.innerText++
+      break;
+
+    case 'decr':
+      number.innerText--
+      break;
+  }
+
 };
 
 makeElement('button', '+', 'buttonPlus');
 makeElement('div', 0, 'number');
 makeElement('button', '-', 'buttonMinus');
+
+number = document.getElementById('number')
+
+document.getElementById('buttonPlus').addEventListener('click', () => changeValue('incr', 1));
