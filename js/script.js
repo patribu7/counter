378f2ab1e5ed4607bea5056f7e9ad5c0fc552be6
patribu7 <patribu7@gmail.com>
2022-08-03 18:09:11 +0200

    Memorize number
    -fix some issues
    -makeElement add property class too

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 let keyboard = []
 let counterButton = 0;
 
-function makeElement(type, text, id, container) {
+/*aggiungere la classe (spero che se vuota la salti) e le misure*/
+function makeElement(type, text, id, parentId) {
   const element = document.createElement(type);
+  const parent = document.getElementById(parentId);
+
   element.innerText = text;
   element.setAttribute('id', id);
-  document.getElementById(container).appendChild(element);
+  parent.appendChild(element);
 
-}
+  if (parent.classList.contains('list')) {
+    element.classList.add('memoElm')
+    element.setAttribute('style','width: 50px')
+
+  }
+
+};
+
+/*aggiungere una funzione che assegni una classe del colore giusto se negativo o positivo*/
 
 function changeValue(value) {
       number.innerText = parseInt(number.innerText) + value
 };
 
-makeElement('button', '+', 'plus', 'counter');
-makeElement('div', 0, 'number', 'counter');
 makeElement('button', '-', 'minus', 'counter');
+makeElement('div', 0, 'number', 'counter');
+makeElement('button', '+', 'plus', 'counter');
 
 number = document.getElementById('number');
 buttonPlus = document.getElementById('plus');
@@ -27,5 +38,7 @@ minus.addEventListener('click', () => changeValue(-1));
 // funzioni aggiunte per mostrare la riusabilita' del codice
 // opzioni a scomparsa
 
-customButtonSettup = document.getElementById('customButtonSettup')
-document.getElementById('makeButton').addEventListener('click', () => customButtonSettup.style.display = 'block')
+customButtonSetup = document.getElementById('customButtonSetup');
+document.getElementById('openSetup').addEventListener('click', () => customButtonSetup.style.display = 'block');
+
+document.getElementById('save').addEventListener('click', () => makeElement('div', number.innerText, '', 'listMemo'))
