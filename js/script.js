cec430f3b384982c162dd2a118b54b9ca4249924
patribu7 <patribu7@gmail.com>
2022-08-03 09:29:34 +0200

    Add assign container to f() makeElement
    Clean test button

--- js/script.js
+++ js/script.js
@@ -1,37 +1,27 @@
-function makeElement(type, text, id) {
+let keyboard = []
+let counterButton = 0;
+
+function makeElement(type, text, id, container) {
   const element = document.createElement(type);
   element.innerText = text;
   element.setAttribute('id', id);
-  document.getElementById('container').appendChild(element);
-};
+  document.getElementById(container).appendChild(element);
+
+}
 
 function changeValue(value) {
       number.innerText = parseInt(number.innerText) + value
 };
 
-makeElement('button', '+', 'buttonPlus');
-makeElement('div', 0, 'number');
-makeElement('button', '-', 'buttonMinus');
+makeElement('button', '+', 'plus', 'counter');
+makeElement('div', 0, 'number', 'counter');
+makeElement('button', '-', 'minus', 'counter');
 
 number = document.getElementById('number');
-buttonPlus = document.getElementById('buttonPlus');
-buttonMinus = document.getElementById('buttonMinus');
+buttonPlus = document.getElementById('plus');
+buttonMinus = document.getElementById('minus');
 
-buttonPlus.addEventListener('click', () => changeValue(1));
-buttonMinus.addEventListener('click', () => changeValue(-1));
+plus.addEventListener('click', () => changeValue(1));
+minus.addEventListener('click', () => changeValue(-1));
 
 // funzioni aggiunte per mostrare la riusabilita' del codice
-counterButton = 0
-
-makeElement('button', '+10', 'buttonPlus10');
-makeElement('button', '-10', 'buttonMinus10');
-makeElement('button', 'make your own button!', 'buttonAdd')
-
-
-buttonPlus10 = document.getElementById('buttonPlus10');
-buttonMinus10 = document.getElementById('buttonMinus10');
-buttonAdd = document.getElementById('buttonAdd');
-
-buttonPlus10.addEventListener('click', () => changeValue(10));
-buttonMinus10.addEventListener('click', () => changeValue(-10));
-buttonAdd.addEventListener('click', () => makeElement('button', number.innerText, 'customButton' + ++counterButton));
