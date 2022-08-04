969ab770091c617881a01a87adcaefd35c06acec
patribu7 <patribu7@gmail.com>
2022-08-04 21:39:51 +0200

    Set color for positive or negative values

--- js/script.js
+++ js/script.js
@@ -18,6 +18,17 @@ function makeElement(type, text, id, parentId) {
     element.addEventListener('click', () => changeValue(+text));
   };
 
+  if (+element.innerText > 0) {
+    element.classList.add('positive');
+
+  } else if (+element.innerText < 0) {
+    element.classList.add('negative');
+
+  } else if (+element.innerText === 0) {
+    element.classList.add('zero');
+
+  };
+
 };
 
 /*aggiungere una funzione che assegni una classe del colore giusto se negativo o positivo*/
@@ -34,12 +45,16 @@ number = document.getElementById('number');
 buttonPlus = document.getElementById('plus');
 buttonMinus = document.getElementById('minus');
 
+//assegno il colore
+buttonPlus.classList.add('positive');
+buttonMinus.classList.add('negative');
+
+//assegno la funzione
 plus.addEventListener('click', () => changeValue(1));
 minus.addEventListener('click', () => changeValue(-1));
 
-// funzioni aggiunte per mostrare la riusabilita' del codice
-// opzioni a scomparsa
 
+// opzioni a scomparsa
 customButtonSetup = document.getElementById('customButtonSetup');
 document.getElementById('openSetup').addEventListener('click', () => customButtonSetup.style.display = 'block');
 
